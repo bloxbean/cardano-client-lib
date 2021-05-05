cb129d3c91bfbb105e505c1d1fcb54116454eee9
Satya <satran004@gmail.com>
2021-05-05 23:15:30 +0800

    TransactionInput code cleanup

--- src/main/java/com/bloxbean/cardano/client/transaction/model/TransactionInput.java
+++ src/main/java/com/bloxbean/cardano/client/transaction/model/TransactionInput.java
@@ -1,13 +1,7 @@
 package com.bloxbean.cardano.client.transaction.model;
 
-import co.nstant.in.cbor.CborDecoder;
 import co.nstant.in.cbor.CborException;
 import co.nstant.in.cbor.builder.ArrayBuilder;
-import co.nstant.in.cbor.model.Array;
-import co.nstant.in.cbor.model.DataItem;
-
-import java.io.ByteArrayInputStream;
-import java.util.List;
 
 public class TransactionInput {
     private byte[] transaction_id;
@@ -33,32 +27,5 @@ public class TransactionInput {
         builder.add(transaction_id)
                 .add(index)
                 .end();
-//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//        new CborEncoder(baos).encode(new CborBuilder()
-//                .addArray()
-////                .add()
-////                .add(2)
-//                .add(transaction_id)
-//                .add(index)
-//                .end()
-//
-//                .build());
-//        byte[] encodedBytes = baos.toByteArray();
-//        return encodedBytes;
-    }
-
-    public static TransactionInput deserialize(byte[] bytes) throws CborException {
-        ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
-        List<DataItem> dataItems = new CborDecoder(bais).decode();
-        System.out.println(dataItems.size());
-        for(DataItem dataItem : dataItems) {
-            // process data item
-            if(dataItem instanceof Array) {
-                Array array = (Array) dataItem;
-                List<DataItem> items = array.getDataItems();
-
-            }
-        }
-        return null;
     }
 }
