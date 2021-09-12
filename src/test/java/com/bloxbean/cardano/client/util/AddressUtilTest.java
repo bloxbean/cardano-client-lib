39e9122237e8878e2e2ed0a7133603f316c37533
Mateusz Czeladka <mateusz.szczap[at]gmail.com>
2021-09-12 18:30:51 +0200

    Issue: 14, add AddressUtil to validate addresses

---
+++ src/test/java/com/bloxbean/cardano/client/util/AddressUtilTest.java
@@ -0,0 +1,45 @@
+package com.bloxbean.cardano.client.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AddressUtilTest {
+
+    @Test
+    public void testValidShelleyAddress() {
+        boolean isValid = AddressUtil.isValidAddress("addr1qxkeutm43mhc8jpqg6sk4cqtypzy3ez6z8k7qlfwa97h2acz7xprvuysll04e5gaa65vavyj0wvd0v99lhpntm7c03us8wk6xc");
+
+        assertTrue(isValid);
+    }
+
+    @Test
+    public void testInvalidShelleyAddr() {
+        boolean isValid = AddressUtil.isValidAddress("addr1qxkeutm43mhc8jpqg6sk4cqtypzy3ez6z8k7qlfwa97h2acz7xprvuysll04e5gaa65vavyj0wvd0v99lhpntm7c03us8wk6xa");
+
+        assertFalse(isValid);
+    }
+
+    @Test
+    public void testInvalidJunk() {
+        boolean isValid = AddressUtil.isValidAddress("some_text");
+
+        assertFalse(isValid);
+    }
+
+    //@Test
+    //FIXME
+    public void testValidByronAddr() {
+        boolean isValid = AddressUtil.isValidAddress("DdzFFzCqrhszg6cqZvDhEwUX7cZyNzdycAVpm4Uo2vjKMgTLrVqiVKi3MBt2tFAtDe7NkptK6TAhVkiYzhavmKV5hE79CWwJnPCJTREK");
+
+        assertTrue(isValid);
+    }
+
+    @Test
+    public void testInvalidByronAddr() {
+        boolean isValid = AddressUtil.isValidAddress("DdzFFzCqrhszg6cqZvDhEwUX7cZyNzdycAVpm4Uo2vjKMgTLrVqiVKi3MBt2tFAtDe7NkptK6TAhVkiYzhavmKV5hE79CWwJnPCJTREk");
+
+        assertFalse(isValid);
+    }
+
+}
