e69e732eb2c052553b7026812381ee9eed5ba419
Andy Pfister <andy.pfister@hey.com>
2021-10-05 22:14:30 +0200

    Improve library util, add support for Android

--- src/main/java/com/bloxbean/cardano/client/util/LibraryUtil.java
+++ src/main/java/com/bloxbean/cardano/client/util/LibraryUtil.java
@@ -1,38 +1,21 @@
 package com.bloxbean.cardano.client.util;
 
+import com.sun.jna.Platform;
+
 public class LibraryUtil {
-
-    private static String OS = System.getProperty("os.name").toLowerCase();
-
     public static String getCardanoWrapperLib() {
         String libName = "libcardano_jni_wrapper";
-        if(isMac()) {
+
+        if (Platform.isMac()) {
             libName += ".dylib";
-        } else if(isUnix()) {
+        } else if (Platform.isAndroid()) {
+            libName = "cardano_jni_wrapper";
+        } else if (Platform.isLinux() || Platform.isFreeBSD() || Platform.isAIX()) {
             libName += ".so";
-        } else if(isWindows()) {
+        } else if (Platform.isWindows() || Platform.isWindowsCE()) {
             libName = "cardano_jni_wrapper.dll";
         }
 
         return libName;
     }
-
-    public static boolean isWindows() {
-        return (OS.indexOf("win") >= 0);
-    }
-
-    public static boolean isMac() {
-        return (OS.indexOf("mac") >= 0);
-    }
-
-    public static boolean isUnix() {
-        return (OS.indexOf("nix") >= 0
-                || OS.indexOf("nux") >= 0
-                || OS.indexOf("aix") > 0);
-    }
-
-    public static boolean isSolaris() {
-        return (OS.indexOf("sunos") >= 0);
-    }
-
 }
