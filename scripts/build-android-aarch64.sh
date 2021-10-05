3cc968104aa55f44a8fca2cc0568c6791f54e34b
Andy Pfister <andy.pfister@hey.com>
2021-10-05 19:00:04 +0200

    Build library against Android

---
+++ scripts/build-android-aarch64.sh
@@ -0,0 +1,13 @@
+export SRC_LIB_FILE=libcardano_jni_wrapper.so
+export TARGET_LIB_FILE=aarch64-linux-android_libcardano_jni_wrapper.so
+export NATIVE_FOLDER=armv7-linux-android
+
+cd rust
+cargo ndk -t arm64-v8a -o ./jniLibs build --release
+cp jniLibs/arm64-v8a/$SRC_LIB_FILE target/release/$TARGET_LIB_FILE
+cd ..
+
+mkdir -p native/$NATIVE_FOLDER
+cp rust/target/release/$SRC_LIB_FILE native/$NATIVE_FOLDER
+
+ls native/$NATIVE_FOLDER && pwd
---
+++ scripts/build-android-armv7.sh
@@ -0,0 +1,13 @@
+export SRC_LIB_FILE=libcardano_jni_wrapper.so
+export TARGET_LIB_FILE=armv7-linux-androideabi_libcardano_jni_wrapper.so
+export NATIVE_FOLDER=armv7-linux-androideabi
+
+cd rust
+cargo ndk -t armeabi-v7a -o ./jniLibs build --release
+cp jniLibs/armeabi-v7a/$SRC_LIB_FILE target/release/$TARGET_LIB_FILE
+cd ..
+
+mkdir -p native/$NATIVE_FOLDER
+cp rust/target/release/$SRC_LIB_FILE native/$NATIVE_FOLDER
+
+ls native/$NATIVE_FOLDER && pwd
