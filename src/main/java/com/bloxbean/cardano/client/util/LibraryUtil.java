beae14c98445e199b7d9cc69be0669dd96cc295e
Satya <satran004@gmail.com>
2021-10-09 13:42:22 +0800

    [#18] Caller can provide native lib name through system properties or env variable. (cardano_client_native_lib_name)

--- src/main/java/com/bloxbean/cardano/client/util/LibraryUtil.java
+++ src/main/java/com/bloxbean/cardano/client/util/LibraryUtil.java
@@ -3,7 +3,13 @@ package com.bloxbean.cardano.client.util;
 import com.sun.jna.Platform;
 
 public class LibraryUtil {
+    public static final String CARDANO_CLIENT_NATIVE_LIB_NAME = "cardano_client_native_lib_name";
+
     public static String getCardanoWrapperLib() {
+        String customNativeLib = getCustomNativeLibName();
+        if(customNativeLib != null && !customNativeLib.isEmpty())
+            return customNativeLib;
+
         String libName = "libcardano_jni_wrapper";
 
         if (Platform.isMac()) {
@@ -17,5 +23,14 @@ public class LibraryUtil {
         }
 
         return libName;
+    }
+
+    private static String getCustomNativeLibName() {
+        String nativeLibName = System.getProperty(CARDANO_CLIENT_NATIVE_LIB_NAME);
+        if(nativeLibName == null || nativeLibName.isEmpty()) {
+            nativeLibName = System.getenv(CARDANO_CLIENT_NATIVE_LIB_NAME);
+        }
+
+        return nativeLibName;
     }
 }
