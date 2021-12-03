9ed78f8fc11a63dcbb604664801ecd0fe98866d9
Satya <satran004@gmail.com>
2021-12-03 18:10:12 +0800

    Contract call test

--- src/integration-test/java/com/bloxbean/cardano/client/backend/api/helper/ContractTransactionIT.java
+++ src/integration-test/java/com/bloxbean/cardano/client/backend/api/helper/ContractTransactionIT.java
@@ -68,7 +68,9 @@ public class ContractTransactionIT extends BFBaseTest {
         BigInteger scriptAmt = new BigInteger("2479280");
         String scriptAddress = "addr_test1wpnlxv2xv9a9ucvnvzqakwepzl9ltx7jzgm53av2e9ncv4sysemm8";
 
-        PlutusData plutusData = new BigIntPlutusData(new BigInteger("2021"));
+        Random rand = new Random();
+        int randInt = rand.nextInt();
+        PlutusData plutusData = new BigIntPlutusData(BigInteger.valueOf(randInt));
         String datumHash = plutusData.getDatumHash();
 
         //Transfer fund to script address
@@ -94,14 +96,10 @@ public class ContractTransactionIT extends BFBaseTest {
                 .transactionId(collateral)
                 .index(collateralIndex).build();
 
-
-        BigInteger fee = BigInteger.valueOf(323456);
-        BigInteger changeAmt = scriptAmt.subtract(fee);
-
         TransactionOutput change = TransactionOutput
                 .builder()
                 .address(sender.baseAddress())
-                .value(new Value(changeAmt, null))
+                .value(new Value(scriptAmt, null)) //Actual amount will be set after fee estimation
                 .build();
 
         List<TransactionOutput> outputs = Arrays.asList(change);
@@ -116,8 +114,8 @@ public class ContractTransactionIT extends BFBaseTest {
                 .inputs(inputs)
                 .outputs(outputs)
                 .collateral(Arrays.asList(collateralInput))
+                .fee(BigInteger.valueOf(170000)) //Dummy fee
                 .ttl(getTtl())
-                .fee(fee)
                 .networkId(NetworkId.TESTNET)
                 .build();
 
@@ -155,15 +153,26 @@ public class ContractTransactionIT extends BFBaseTest {
                 .body(body)
                 .witnessSet(transactionWitnessSet)
                 .auxiliaryData(auxiliaryData)
-                .isValid(true)
                 .build();
 
-        String hex = transaction.serializeToHex();
-        System.out.println(hex);
-        //Sign the transaction. so that we get the actual size of the transaction to calculate the fee
-        String signTxnHash = sender.sign(transaction); //cbor encoded bytes in Hex format
+        System.out.println(transaction);
+        String signTxnHashForFeeCalculation = sender.sign(transaction);
 
-        //Calculate fees
+        BigInteger baseFee = feeCalculationService.calculateFee(HexUtil.decodeHexString(signTxnHashForFeeCalculation));
+        BigInteger scriptFee = feeCalculationService.calculateScriptFee(Arrays.asList(redeemer.getExUnits()));
+        BigInteger totalFee = baseFee.add(scriptFee);
+
+        System.out.println("Total Fee ----- " + totalFee);
+
+        //Update change amount based on fee
+        BigInteger changeAmt = scriptAmt.subtract(totalFee);
+        change.getValue().setCoin(changeAmt);
+        body.setFee(totalFee);
+
+        System.out.println("-- fee : " + totalFee );
+
+        String signTxnHash = sender.sign(transaction); //cbor encoded bytes in Hex format
+        System.out.println(signTxnHash);
         byte[] signTxnBytes = HexUtil.decodeHexString(signTxnHash);
 
         Result<String> result = transactionService.submitTransaction(signTxnBytes);
@@ -172,78 +181,219 @@ public class ContractTransactionIT extends BFBaseTest {
         waitForTransaction(result);
     }
 
-//    @Test
-//    public void spaceBudzBid() throws CborSerializationException, AddressExcepion, ApiException, CborDeserializationException, CborException {
-//        String scriptAddress = "addr1wyzynye0nksztrfzpsulsq7whr3vgh7uvp0gm4p0x42ckkqqq6kxq";
+    /*
+    @Test
+    public void spaceBudzBid() throws CborSerializationException, AddressExcepion, ApiException, CborDeserializationException, CborException {
+        String scriptAddress = "addr1wyzynye0nksztrfzpsulsq7whr3vgh7uvp0gm4p0x42ckkqqq6kxq";
+        String sender = "";
+
+        Utxo collateralUtxo = Utxo.builder()
+                .txHash("a720266b8c1865558560c5f40e299f6c072834201eb80a273cad5a3fad9b050d")
+                .outputIndex(0)
+                .build();
+
+        //Tuple t = AssetUtil.getPolicyIdAndAssetName("800df05a0cc6b6f0d28aaa1812135bd9eebfbf5e8e80fd47da9989eb53706163654275644269643530");
+        String policyId = "800df05a0cc6b6f0d28aaa1812135bd9eebfbf5e8e80fd47da9989eb";
+        String assetName = HexUtil.encodeHexString("SpaceBudBid2413".getBytes(StandardCharsets.UTF_8));
+        String unit = policyId + assetName;
+
+        UtxoSelectionStrategy utxoSelectionStrategy = new DefaultUtxoSelectionStrategyImpl(utxoService);
+        utxoSelectionStrategy.setIgnoreUtxosWithDatumHash(false);
+        List<Utxo> utxos = utxoSelectionStrategy.selectUtxos(scriptAddress, unit, BigInteger.valueOf(1), Collections.EMPTY_SET);
+
+        Utxo scriptUtxo = utxos.get(0);
+
+        BigInteger bidAmount = BigInteger.valueOf(10000000);
+        List<Utxo> senderUtxos = utxoSelectionStrategy.selectUtxos(sender, LOVELACE, bidAmount, Set.of(collateralUtxo));
+
+        List<MetadataJSONContent> metadataList = metadataService.getJSONMetadataByTxnHash(scriptUtxo.getTxHash()).getValue();
+
+        String datum = metadataList.stream().filter( metadataJSONContent -> metadataJSONContent.getLabel().equals("405"))
+                .findFirst()
+                .get().getJsonMetadata()
+                .get(scriptUtxo.getOutputIndex() + "").asText();
+
+        ConstrPlutusData START_BID = ConstrPlutusData.builder()
+                .tag(0)
+                .data(new ListPlutusData())
+                .build();
+        String START_BID_HEX = START_BID.getDatumHash();
+
+        System.out.println(datum);
+        String currentOwner = null;
+        if (!datum.equals(START_BID_HEX)) {
+            String currentOwnerHex = metadataList.stream().filter( metadataJSONContent -> metadataJSONContent.getLabel().equals("406"))
+                    .findFirst()
+                    .get().getJsonMetadata()
+                    .get("address").asText();
+
+            currentOwner = Account.bytesToBech32(HexUtil.decodeHexString(currentOwnerHex));
+        }
+
+        System.out.println(currentOwner);
+
+        if (!PlutusUtil.hashPlutusData(datum).equals(scriptUtxo.getDataHash())) {
+            throw new RuntimeException("Datum hash doesn't match");
+        }
+
+        System.out.println("Done");
+
+        PlutusData redeemerData = ConstrPlutusData.builder()
+                .tag(2)
+                .data(new ListPlutusData())
+                .build();
+
+        Redeemer redeemer = Redeemer.builder()
+                .tag(RedeemerTag.Spend)
+                .data(redeemerData)
+                .index(BigInteger.valueOf(0))
+                .exUnits(ExUnits.builder()
+                        .mem(BigInteger.valueOf(7000000))
+                        .steps(BigInteger.valueOf(3000000000L)).build()
+                ).build();
+
 //
-//        //Tuple t = AssetUtil.getPolicyIdAndAssetName("800df05a0cc6b6f0d28aaa1812135bd9eebfbf5e8e80fd47da9989eb53706163654275644269643530");
-//        String policyId = "800df05a0cc6b6f0d28aaa1812135bd9eebfbf5e8e80fd47da9989eb";
-//        String assetName = HexUtil.encodeHexString("SpaceBudBid2413".getBytes(StandardCharsets.UTF_8));
-//        String unit = policyId + assetName;
+        List<TransactionInput> inputs = new ArrayList<>();
+
+        //script utxo
+        TransactionInput input = TransactionInput.builder()
+                .transactionId(scriptUtxo.getTxHash())
+                .index(scriptUtxo.getOutputIndex())
+                .build();
+
+        //sender utxos
+        senderUtxos.forEach(utxo -> {
+            TransactionInput transactionInput = TransactionInput.builder()
+                    .transactionId(utxo.getTxHash())
+                    .index(utxo.getOutputIndex())
+                    .build();
+
+            inputs.add(transactionInput);
+        });
+
+        //New script output
+        TransactionOutput output = TransactionOutput.builder()
+                .address(scriptAddress)
+                .value(Value.builder()
+                        .coin(BigInteger.ZERO)
+                        .build())
+                .build();
+        copyUtxoValuesToChangeOutput(output, scriptUtxo);
+
+        //Change output
+        TransactionOutput changeOutput = TransactionOutput.builder()
+                .address(sender)
+                .value(Value.builder()
+                    .coin(BigInteger.ZERO)
+                    .build())
+                .build();
+
+        //Copy selected utxos content to transactionoutput
+        senderUtxos.forEach(utxo -> copyUtxoValuesToChangeOutput(changeOutput, utxo));
+
+        String tradeOwner = HexUtil.encodeHexString(Account.toBytes(sender));
+
+        //BID datum
+        ListPlutusData listPlutusData = new ListPlutusData();
+        listPlutusData.add(new BytesPlutusData(HexUtil.decodeHexString(tradeOwner)));
+        listPlutusData.add(new BytesPlutusData());
+        PlutusData bidDatum = ConstrPlutusData.builder()
+                .tag(1)
+                .data()
+                .build();
+
+        //const metadata = { [DATUM_LABEL]: {}, [ADDRESS_LABEL]: {} };
+        CBORMetadataMap datumLabelMap = new CBORMetadataMap();
+        datumLabelMap.put("0", "0x" + HexUtil.encodeHexString(datum));
+
+        CBORMetadataMap addressLabelMap = new CBORMetadataMap();
+        addressLabelMap.put("0", "0x" + HexUtil.encodeHexString(Account.toBytes(sender)));
+
+        CBORMetadata metadata = new CBORMetadata();
+        metadata.put(BigInteger.valueOf(405), datumLabelMap);
+        metadata.put(BigInteger.valueOf(406), addressLabelMap);
+//        const DATUM_TYPE = {
+//                StartBid: 0,
+//                Bid: 1,
+//                Offer: 2,
+//        };
+
+//        String tradeOwnerAddress =
+
+//        ListPlutusData bidListPlutusData = ListPlutusData.builder()
+//                .plutusDataList(Arrays.asList(
 //
-//        UtxoSelectionStrategy utxoSelectionStrategy = new DefaultUtxoSelectionStrategyImpl(utxoService);
-//        utxoSelectionStrategy.setIgnoreUtxosWithDatumHash(false);
-//        List<Utxo> utxos = utxoSelectionStrategy.selectUtxos(scriptAddress, unit, BigInteger.valueOf(1), Collections.EMPTY_SET);
+//                )).build();
 //
-//        Utxo scriptUtxo = utxos.get(0);
 //
-//        List<MetadataJSONContent> metadataList = metadataService.getJSONMetadataByTxnHash(scriptUtxo.getTxHash()).getValue();
+//        ConstrPlutusData BID = ConstrPlutusData.builder()
+
+//        BigIntPlutusData bytesPlutusData = new BigIntPlutusData(BigInteger.valueOf(0));
+//        ListPlutusData listPlutusData = new ListPlutusData();
 //
-//        String datum = metadataList.stream().filter( metadataJSONContent -> metadataJSONContent.getLabel().equals("405"))
-//                .findFirst()
-//                .get().getJsonMetadata()
-//                .get(scriptUtxo.getOutputIndex() + "").asText();
-//
-//        ConstrPlutusData START_BID = ConstrPlutusData.builder()
+//        ConstrPlutusData constrPlutusData = ConstrPlutusData.builder()
 //                .alternative(0)
-//                .data(new ListPlutusData())
+//                .data(listPlutusData)
 //                .build();
-//        String START_BID_HEX = START_BID.getDatumHash();
 //
-//        System.out.println(datum);
-//        String currentOwner = null;
-//        if (!datum.equals(START_BID_HEX)) {
-//            String currentOwnerHex = metadataList.stream().filter( metadataJSONContent -> metadataJSONContent.getLabel().equals("406"))
-//                    .findFirst()
-//                    .get().getJsonMetadata()
-//                    .get("address").asText();
+//        String datumHash = constrPlutusData.getDatumHash();
 //
-//            currentOwner = Account.bytesToBech32(HexUtil.decodeHexString(currentOwnerHex));
+//        String datumHex = HexUtil.encodeHexString(CborSerializationUtil.serialize(constrPlutusData.serialize()));
+//
+//        System.out.println(datumHash);
+//        System.out.println(datumHex);
+
+    }
+*/
+//    /**
+//     * Copy utxo content to TransactionOutput
+//     * @param changeOutput
+//     * @param utxo
+//     */
+//  private void copyUtxoValuesToChangeOutput(TransactionOutput changeOutput, Utxo utxo) {
+//        utxo.getAmount().forEach(utxoAmt -> { //For each amt in utxo
+//            String utxoUnit = utxoAmt.getUnit();
+//            BigInteger utxoQty = utxoAmt.getQuantity();
+//            if (utxoUnit.equals(LOVELACE)) {
+//                BigInteger existingCoin = changeOutput.getValue().getCoin();
+//                if (existingCoin == null) existingCoin = BigInteger.ZERO;
+//                changeOutput.getValue().setCoin(existingCoin.add(utxoQty));
+//            } else {
+//                Tuple<String, String> policyIdAssetName = AssetUtil.getPolicyIdAndAssetName(utxoUnit);
+//
+//                //Find if the policy id is available
+//                Optional<MultiAsset> multiAssetOptional =
+//                        changeOutput.getValue().getMultiAssets().stream().filter(ma -> policyIdAssetName._1.equals(ma.getPolicyId())).findFirst();
+//                if (multiAssetOptional.isPresent()) {
+//                    Optional<Asset> assetOptional = multiAssetOptional.get().getAssets().stream()
+//                            .filter(ast -> policyIdAssetName._2.equals(ast.getName()))
+//                            .findFirst();
+//                    if (assetOptional.isPresent()) {
+//                        BigInteger changeVal = assetOptional.get().getValue().add(utxoQty);
+//                        assetOptional.get().setValue(changeVal);
+//                    } else {
+//                        Asset asset = new Asset(policyIdAssetName._2, utxoQty);
+//                        multiAssetOptional.get().getAssets().add(asset);
+//                    }
+//                } else {
+//                    Asset asset = new Asset(policyIdAssetName._2, utxoQty);
+//                    MultiAsset multiAsset = new MultiAsset(policyIdAssetName._1, new ArrayList<>(Arrays.asList(asset)));
+//                    changeOutput.getValue().getMultiAssets().add(multiAsset);
+//                }
+//            }
+//        });
+//
+//        //Remove any empty MultiAssets
+//        List<MultiAsset> multiAssets = changeOutput.getValue().getMultiAssets();
+//        List<MultiAsset> markedForRemoval = new ArrayList<>();
+//        if(multiAssets != null && multiAssets.size() > 0) {
+//            multiAssets.forEach(ma -> {
+//                if(ma.getAssets() == null || ma.getAssets().size() == 0)
+//                    markedForRemoval.add(ma);
+//            });
+//
+//            if (markedForRemoval != null && !markedForRemoval.isEmpty()) multiAssets.removeAll(markedForRemoval);
 //        }
-//
-//        System.out.println(currentOwner);
-//
-////        const DATUM_TYPE = {
-////                StartBid: 0,
-////                Bid: 1,
-////                Offer: 2,
-////        };
-//
-////        String tradeOwnerAddress =
-//
-////        ListPlutusData bidListPlutusData = ListPlutusData.builder()
-////                .plutusDataList(Arrays.asList(
-////
-////                )).build();
-////
-////
-////        ConstrPlutusData BID = ConstrPlutusData.builder()
-//
-////        BigIntPlutusData bytesPlutusData = new BigIntPlutusData(BigInteger.valueOf(0));
-////        ListPlutusData listPlutusData = new ListPlutusData();
-////
-////        ConstrPlutusData constrPlutusData = ConstrPlutusData.builder()
-////                .alternative(0)
-////                .data(listPlutusData)
-////                .build();
-////
-////        String datumHash = constrPlutusData.getDatumHash();
-////
-////        String datumHex = HexUtil.encodeHexString(CborSerializationUtil.serialize(constrPlutusData.serialize()));
-////
-////        System.out.println(datumHash);
-////        System.out.println(datumHex);
-//
 //    }
 
     private boolean transferToContractAddress(Account sender, String scriptAddress, BigInteger amount, String datumHash,
