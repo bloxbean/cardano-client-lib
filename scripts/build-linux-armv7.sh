01351034a63024e24a52ccb67369e76e0d630b44
Satya <satran004@gmail.com>
2021-12-05 01:50:04 +0800

    Yml syntax fix

--- scripts/build-linux-armv7.sh
+++ scripts/build-linux-armv7.sh
@@ -1,10 +1,11 @@
 export SRC_LIB_FILE=libcardano_jni_wrapper.so
-export TARGET_LIB_FILE=linux-arm_libcardano_jni_wrapper.so
+export TARGET_LIB_FILE=linux-armv7_libcardano_jni_wrapper.so
 export NATIVE_FOLDER=linux-arm
 export RUST_TARGET=armv7-unknown-linux-gnueabihf
 
 cd rust
 cargo build --all --release --target $RUST_TARGET
+ls -ltr target/$RUST_TARGET/release/*
 cp target/$RUST_TARGET/release/$SRC_LIB_FILE target/$RUST_TARGET/release/$TARGET_LIB_FILE
 cd ..
 
