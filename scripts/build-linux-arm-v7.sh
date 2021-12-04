0175dffba71fee429f464285e595fe7329180602
Satya <satran004@gmail.com>
2021-12-05 00:06:06 +0800

    Try again. Fixed build for aarch64 and armv7

--- scripts/build-linux-arm-v7.sh
+++ scripts/build-linux-arm-v7.sh
@@ -1,7 +1,7 @@
 export SRC_LIB_FILE=libcardano_jni_wrapper.so
-export TARGET_LIB_FILE=linux-aarch64_libcardano_jni_wrapper.so
-export NATIVE_FOLDER=linux-aarch64
-export RUST_TARGET=aarch64-unknown-linux-gnu
+export TARGET_LIB_FILE=linux-arm_libcardano_jni_wrapper.so
+export NATIVE_FOLDER=linux-arm
+export RUST_TARGET=armv7-unknown-linux-gnueabihf
 
 cd rust
 cargo build --all --release --target $RUST_TARGET
@@ -12,4 +12,3 @@ mkdir -p native/$NATIVE_FOLDER
 cp rust/target/$RUST_TARGET/release/$SRC_LIB_FILE native/$NATIVE_FOLDER
 
 ls native/$NATIVE_FOLDER && pwd
-
