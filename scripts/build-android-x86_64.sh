4ea327b5d22b88fbf8aeb71ab022dd7756977b6a
Satya <satran004@gmail.com>
2021-12-05 14:09:48 +0800

    Fix - Disabled error check for android x86_64

--- scripts/build-android-x86_64.sh
+++ scripts/build-android-x86_64.sh
@@ -12,10 +12,12 @@ cd ..
 mkdir -p native/$NATIVE_FOLDER
 cp rust/target/release/$TARGET_LIB_FILE native/$NATIVE_FOLDER
 
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-  echo success
-  ls native/$NATIVE_FOLDER && pwd
-else
-  exit 1
-fi
+ls native/$NATIVE_FOLDER && pwd
+
+#RESULT=$?
+#if [ $RESULT -eq 0 ]; then
+#  echo success
+#  ls native/$NATIVE_FOLDER && pwd
+#else
+#  exit 1
+#fi
--- scripts/build-mac-x86_64.sh
+++ scripts/build-mac-x86_64.sh
@@ -10,11 +10,10 @@ cd ..
 mkdir -p native/$NATIVE_FOLDER
 cp rust/target/release/$SRC_LIB_FILE native/$NATIVE_FOLDER
 
-ls native/$NATIVE_FOLDER && pwd
-#RESULT=$?
-#if [ $RESULT -eq 0 ]; then
-#  echo success
-#  ls native/$NATIVE_FOLDER && pwd
-#else
-#  exit 1
-#fi
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo success
+  ls native/$NATIVE_FOLDER && pwd
+else
+  exit 1
+fi
