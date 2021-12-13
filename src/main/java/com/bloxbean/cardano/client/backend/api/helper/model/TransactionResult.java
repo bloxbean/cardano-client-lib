96ea099ca74ff52ab59c742e21a5b09ac85376ca
Satya <satran004@gmail.com>
2021-12-13 23:57:59 +0800

    toString() impl

--- src/main/java/com/bloxbean/cardano/client/backend/api/helper/model/TransactionResult.java
+++ src/main/java/com/bloxbean/cardano/client/backend/api/helper/model/TransactionResult.java
@@ -1,5 +1,6 @@
 package com.bloxbean.cardano.client.backend.api.helper.model;
 
+import com.bloxbean.cardano.client.util.HexUtil;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,4 +11,16 @@ import lombok.NoArgsConstructor;
 public class TransactionResult {
     private byte[] signedTxn;
     private String transactionId;
+
+    @Override
+    public String toString() {
+        if (signedTxn != null) {
+            return "TransactionResult{" +
+                    "signedTxn=" + HexUtil.encodeHexString(signedTxn) +
+                    ", transactionId='" + transactionId + '\'' +
+                    '}';
+        } else {
+            return super.toString();
+        }
+    }
 }
