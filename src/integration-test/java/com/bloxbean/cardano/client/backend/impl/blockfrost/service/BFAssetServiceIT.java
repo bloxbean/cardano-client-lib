3de86ab5078417fa7573d9eae98a52c5fde56be8
Satya <satran004@gmail.com>
2021-12-21 20:19:36 +0800

    [#30] Added onchainMetadata field for NFT metadata

--- src/integration-test/java/com/bloxbean/cardano/client/backend/impl/blockfrost/service/BFAssetServiceIT.java
+++ src/integration-test/java/com/bloxbean/cardano/client/backend/impl/blockfrost/service/BFAssetServiceIT.java
@@ -16,7 +16,19 @@ class BFAssetServiceIT extends BFBaseTest {
     @Test
     void getAsset() throws ApiException {
         AssetService service = new BFAssetService(Constants.BLOCKFROST_TESTNET_URL, projectId);
-        Result<Asset> result = service.getAsset("2984b98bab844a0302fed0dab5c787db8f75543f09d9499239e1513674657374746f6b656e");
+        Result<Asset> result = service.getAsset("7d14d344fd85ece5a874b931af0813b57f7496aa61ba1ab7d9097646526566726573684e46545465737431");
+
+        System.out.println(JsonUtil.getPrettyJson(result.getValue()));
+        assertTrue(result.isSuccessful());
+        assertTrue(result.code() == 200);
+        assertNotNull(result.getValue());
+        assertNotNull(result.getValue().getOnchainMetadata());
+    }
+
+    @Test
+    void getAsset1() throws ApiException {
+        AssetService service = new BFAssetService(Constants.BLOCKFROST_TESTNET_URL, projectId);
+        Result<Asset> result = service.getAsset("5c2171471578441ab237b76531539b2d5bfa4193be4aab0466b817f454657374746f6b656e313233");
 
         System.out.println(JsonUtil.getPrettyJson(result.getValue()));
         assertTrue(result.isSuccessful());
--- src/main/java/com/bloxbean/cardano/client/backend/model/Asset.java
+++ src/main/java/com/bloxbean/cardano/client/backend/model/Asset.java
@@ -1,28 +1,57 @@
 package com.bloxbean.cardano.client.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonNaming( PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class Asset {
+    /**
+     * Hex-encoded asset full name
+     */
+    private String asset;
+    /**
+     * Policy ID of the asset
+     */
     private String policyId;
+    /**
+     * Hex-encoded asset name of the asset
+     */
     private String assetName;
+    /**
+     * CIP14 based user-facing fingerprint
+     */
     private String fingerprint;
+    /**
+     * Current asset quantity
+     */
     private String quantity;
+    /**
+     * ID of the initial minting transaction
+     */
     private String initialMintTxHash;
-//    private JsonNullable<Map<String, Object>> onchainMetadata = JsonNullable.<Map<String, Object>>undefined();
-//
-//    public static final String JSON_PROPERTY_METADATA = "metadata";
-//    private AssetMetadata metadata;
+    /**
+     * Count of mint and burn transactions
+     */
+    private Integer mintOrBurnCount;
+    /**
+     * On-chain metadata stored in the minting transaction under label 721,
+     * community discussion around the standard ongoing at
+     * https://github.com/cardano-foundation/CIPs/pull/85
+     */
+    private JsonNode onchainMetadata;
+    /**
+     * Asset metadata
+     */
+    private JsonNode metadata;
 }
