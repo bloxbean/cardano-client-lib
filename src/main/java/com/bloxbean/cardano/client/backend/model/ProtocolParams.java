0b3a379ec28b586cc30fe0ab6d88ee6fa6d87320
dudi <edridudi@gmail.com>
2022-01-05 02:10:42 +0200

    Deprecated PropertyNamingStrategy.SnakeCaseStrategy Change to PropertyNamingStrategies.SnakeCaseStrategy

--- src/main/java/com/bloxbean/cardano/client/backend/model/ProtocolParams.java
+++ src/main/java/com/bloxbean/cardano/client/backend/model/ProtocolParams.java
@@ -2,7 +2,7 @@ package com.bloxbean.cardano.client.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,7 +16,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ProtocolParams {
     private Integer minFeeA;
     private Integer minFeeB;
--- src/main/java/com/bloxbean/cardano/client/backend/model/TxOutputAmount.java
+++ src/main/java/com/bloxbean/cardano/client/backend/model/TxOutputAmount.java
@@ -1,7 +1,7 @@
 package com.bloxbean.cardano.client.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class TxOutputAmount {
     private String unit;
     private String quantity;
