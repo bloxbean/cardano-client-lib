fd37b997aaa8a368defd804a72ef244836f515a3
Satya <satran004@gmail.com>
2022-02-13 23:24:46 +0800

    Javadoc & refactoring

--- src/main/java/com/bloxbean/cardano/client/coinselection/UtxoSelector.java
+++ src/main/java/com/bloxbean/cardano/client/coinselection/UtxoSelector.java
@@ -4,35 +4,40 @@ import com.bloxbean.cardano.client.backend.exception.ApiException;
 import com.bloxbean.cardano.client.backend.model.Utxo;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
 
+/**
+ * Implement this interface to find <code>Utxo</code> by predicate
+ */
 public interface UtxoSelector {
+
     /**
      * Find the first utxo matching the predicate
      * @param address Script address
      * @param predicate Predicate to filter utxos
-     * @return Utxo
-     * @throws ApiException
+     * @return An optional with matching <code>Utxo</code>
+     * @throws ApiException if error
      */
-    Utxo findFirst(String address, Predicate<Utxo> predicate) throws ApiException;
+    Optional<Utxo> findFirst(String address, Predicate<Utxo> predicate) throws ApiException;
 
     /**
      * Find the first utxo matching the predicate
      * @param address Script address
      * @param predicate Predicate to filter utxos
      * @param excludeUtxos Utxos to exclude
-     * @return Utxo
-     * @throws ApiException
+     * @return An optional with matching <code>Utxo</code>
+     * @throws ApiException if error
      */
-    Utxo findFirst(String address, Predicate<Utxo> predicate, Set<Utxo> excludeUtxos) throws ApiException;
+    Optional<Utxo> findFirst(String address, Predicate<Utxo> predicate, Set<Utxo> excludeUtxos) throws ApiException;
 
     /**
      * Find all utxos matching the predicate
      * @param address Script address
      * @param predicate Predicate to filter utxos
      * @return List of Utxos
-     * @throws ApiException
+     * @throws ApiException if error
      */
     List<Utxo> findAll(String address, Predicate<Utxo> predicate) throws ApiException;
 
@@ -42,7 +47,7 @@ public interface UtxoSelector {
      * @param predicate Predicate Predicate to filter utxos
      * @param excludeUtxos Utxos to exclude
      * @return List of Utxos
-     * @throws ApiException
+     * @throws ApiException if error
      */
     List<Utxo> findAll(String address, Predicate<Utxo> predicate, Set<Utxo> excludeUtxos) throws ApiException;
 }
