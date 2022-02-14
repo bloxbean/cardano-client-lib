8bd35bb5fd33738f58d38321b6505b17e915d985
Satya <satran004@gmail.com>
2022-02-14 22:35:29 +0800

    Mint creator tests

--- src/main/java/com/bloxbean/cardano/client/function/helper/MintCreators.java
+++ src/main/java/com/bloxbean/cardano/client/function/helper/MintCreators.java
@@ -2,53 +2,83 @@ package com.bloxbean.cardano.client.function.helper;
 
 import com.bloxbean.cardano.client.function.TxBuilder;
 import com.bloxbean.cardano.client.transaction.spec.*;
+import com.bloxbean.cardano.client.transaction.spec.script.NativeScript;
 
+import java.util.ArrayList;
 import java.util.List;
-
-//TODO -- Unit tests pending
+import java.util.Objects;
 
 /**
- * Provides helper method to transform a {@link Transaction} to add minting related data
+ * Provides helper methods to transform a {@link Transaction} to add minting related data
  */
 public class MintCreators {
 
     /**
      * Function to add mint related data to <code>{@link Transaction}</code>
-     *
+     * <br/>Adds <code>MultiAsset</code> to mint field of <code>TransactionBody</code>.
+     * <br/>Adds policy script to <code>TransactionWitnessSet</code>
+     * <br/>Adds policy script to <code>AuxiliaryData</code>
      * @param policy     Policy instance
      * @param multiAsset MultiAsset to mint
      * @return <code>{@link TxBuilder}</code> function
      */
     public static TxBuilder mintCreator(Policy policy, MultiAsset multiAsset) {
+        return mintCreator(policy, multiAsset, false);
+    }
+
+    /**
+     * Function to add mint related data to <code>{@link Transaction}</code>
+     * <br/>Adds <code>MultiAsset</code> to mint field of <code>TransactionBody</code>.
+     * <br/>Adds policy script to <code>TransactionWitnessSet</code>
+     * <br/>Adds policy script to <code>AuxiliaryData</code>
+     * @param policy     Policy instance
+     * @param multiAsset MultiAsset to mint
+     * @param inclScriptInAuxData Add policy's native script to <code>AuxiliaryData</code> field
+     * @return <code>{@link TxBuilder}</code> function
+     */
+    public static TxBuilder mintCreator(Policy policy, MultiAsset multiAsset, boolean inclScriptInAuxData) {
+        Objects.requireNonNull(policy);
+        Objects.requireNonNull(multiAsset);
 
         return (context, transaction) -> {
+            checkTransactionForNullValuesAndInitializeIfRequired(transaction, inclScriptInAuxData);
+
             if (transaction.getBody().getMint() == null) {
                 transaction.getBody().setMint(List.of(multiAsset));
             } else {
                 transaction.getBody().getMint().add(multiAsset);
             }
 
-            //Set native script
+            NativeScript script = policy.getPolicyScript();
+
+            if (inclScriptInAuxData) {
+                if (!transaction.getAuxiliaryData().getNativeScripts().contains(script))
+                    transaction.getAuxiliaryData().getNativeScripts().add(script);
+            }
+
+            if (!transaction.getWitnessSet().getNativeScripts().contains(script))
+                transaction.getWitnessSet().getNativeScripts().add(script);
+        };
+    }
+
+    private static void checkTransactionForNullValuesAndInitializeIfRequired(Transaction transaction, boolean inclScriptToAuxData) {
+        if (inclScriptToAuxData) {
             AuxiliaryData auxiliaryData = transaction.getAuxiliaryData();
             if (auxiliaryData == null) {
-                auxiliaryData = new AuxiliaryData();
-                transaction.setAuxiliaryData(auxiliaryData);
+                transaction.setAuxiliaryData(new AuxiliaryData());
+                auxiliaryData = transaction.getAuxiliaryData();
             }
 
             if (auxiliaryData.getNativeScripts() == null) {
-                auxiliaryData.setNativeScripts(List.of(policy.getPolicyScript()));
-            } else {
-                auxiliaryData.getNativeScripts().add(policy.getPolicyScript());
+                auxiliaryData.setNativeScripts(new ArrayList<>());
             }
+        }
 
-            if (transaction.getWitnessSet() == null)
-                transaction.setWitnessSet(new TransactionWitnessSet());
+        if (transaction.getWitnessSet() == null)
+            transaction.setWitnessSet(new TransactionWitnessSet());
 
-            if (transaction.getWitnessSet().getNativeScripts() == null)
-                transaction.getWitnessSet().setNativeScripts(List.of(policy.getPolicyScript()));
-            else
-                transaction.getWitnessSet().getNativeScripts().add(policy.getPolicyScript());
-
-        };
+        if (transaction.getWitnessSet().getNativeScripts() == null) {
+            transaction.getWitnessSet().setNativeScripts(new ArrayList<>());
+        }
     }
 }
---
+++ src/test/java/com/bloxbean/cardano/client/function/helper/MintCreatorsTest.java
@@ -0,0 +1,177 @@
+package com.bloxbean.cardano.client.function.helper;
+
+import com.bloxbean.cardano.client.backend.api.BackendService;
+import com.bloxbean.cardano.client.backend.exception.ApiException;
+import com.bloxbean.cardano.client.function.TxBuilderContext;
+import com.bloxbean.cardano.client.transaction.spec.*;
+import com.bloxbean.cardano.client.util.PolicyUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+class MintCreatorsTest {
+
+    @Mock
+    BackendService backendService;
+
+    @BeforeEach
+    public void setup() throws IOException, ApiException {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void mintCreator_whenScriptNotIncludedInAuxData() throws Exception {
+
+        Policy policy1 = PolicyUtil.createMultiSigScriptAllPolicy("policy1", 1);
+        MultiAsset multiAsset1 = MultiAsset.builder()
+                .policyId(policy1.getPolicyId())
+                .assets(List.of(
+                        Asset.builder()
+                                .name("abc")
+                                .value(BigInteger.valueOf(100))
+                                .build()
+                )).build();
+
+        Policy policy2 = PolicyUtil.createMultiSigScriptAllPolicy("policy2", 1);
+        MultiAsset multiAsset2 = MultiAsset.builder()
+                .policyId(policy2.getPolicyId())
+                .assets(List.of(
+                        Asset.builder()
+                                .name("xyz")
+                                .value(BigInteger.valueOf(200))
+                                .build()
+                )).build();
+
+        TxBuilderContext context = new TxBuilderContext(backendService);
+        Transaction transaction = new Transaction();
+
+        //Just adding a random input/output
+        transaction.getBody().getInputs().add(
+                new TransactionInput("735262c68b5fa220dee2b447d0d1dd44e0800ba6212dcea7955c561f365fb0e9", 0)
+        );
+
+        transaction.getBody().getOutputs().add(
+                TransactionOutput.builder()
+                        .address("addr_test1qq46hhhpppek6e33hqakqyu2z5xeqwlc4pc0xynwamn34l6vps306wwr475xeh2lnt4hqjm4mfyjqnvla9j5wtc3fxespv67ka")
+                        .value(Value.builder().coin(BigInteger.valueOf(1000)).build())
+                        .build()
+        );
+
+
+        MintCreators.mintCreator(policy1, multiAsset1)
+                .andThen(MintCreators.mintCreator(policy2, multiAsset2))
+                .accept(context, transaction);
+
+
+        //No update to existing inputs & outputs
+        assertThat(transaction.getBody().getInputs()).hasSize(1);
+        assertThat(transaction.getBody().getOutputs()).hasSize(1);
+
+        assertThat(transaction.getBody().getMint()).contains(multiAsset1, multiAsset2);
+        assertThat(transaction.getWitnessSet().getNativeScripts()).contains(policy1.getPolicyScript(), policy2.getPolicyScript());
+        assertThat(transaction.getAuxiliaryData()).isNull();
+    }
+
+    @Test
+    void mintCreator_whenScriptIncludedInAuxData() throws Exception {
+
+        Policy policy1 = PolicyUtil.createMultiSigScriptAllPolicy("policy1", 1);
+        MultiAsset multiAsset1 = MultiAsset.builder()
+                .policyId(policy1.getPolicyId())
+                .assets(List.of(
+                        Asset.builder()
+                                .name("abc")
+                                .value(BigInteger.valueOf(100))
+                                .build()
+                )).build();
+
+        Policy policy2 = PolicyUtil.createMultiSigScriptAllPolicy("policy2", 1);
+        MultiAsset multiAsset2 = MultiAsset.builder()
+                .policyId(policy2.getPolicyId())
+                .assets(List.of(
+                        Asset.builder()
+                                .name("xyz")
+                                .value(BigInteger.valueOf(200))
+                                .build()
+                )).build();
+
+        TxBuilderContext context = new TxBuilderContext(backendService);
+        Transaction transaction = new Transaction();
+
+        //Just adding a random input/output
+        transaction.getBody().getInputs().add(
+                new TransactionInput("735262c68b5fa220dee2b447d0d1dd44e0800ba6212dcea7955c561f365fb0e9", 0)
+        );
+
+        transaction.getBody().getOutputs().add(
+                TransactionOutput.builder()
+                        .address("addr_test1qq46hhhpppek6e33hqakqyu2z5xeqwlc4pc0xynwamn34l6vps306wwr475xeh2lnt4hqjm4mfyjqnvla9j5wtc3fxespv67ka")
+                        .value(Value.builder().coin(BigInteger.valueOf(1000)).build())
+                        .build()
+        );
+
+
+        MintCreators.mintCreator(policy1, multiAsset1, true)
+                .andThen(MintCreators.mintCreator(policy2, multiAsset2, true))
+                .accept(context, transaction);
+
+
+        //No update to existing inputs & outputs
+        assertThat(transaction.getBody().getInputs()).hasSize(1);
+        assertThat(transaction.getBody().getOutputs()).hasSize(1);
+
+        assertThat(transaction.getBody().getMint()).contains(multiAsset1, multiAsset2);
+        assertThat(transaction.getWitnessSet().getNativeScripts()).contains(policy1.getPolicyScript(), policy2.getPolicyScript());
+        assertThat(transaction.getAuxiliaryData().getNativeScripts()).contains(policy1.getPolicyScript(), policy2.getPolicyScript());
+    }
+
+    @Test
+    void mintCreator_whenSamePolicyIsUsedMultipleTimes_shouldContainsOneCopyOfPolicyScriptInWitness() throws Exception {
+
+        Policy policy1 = PolicyUtil.createMultiSigScriptAllPolicy("policy1", 1);
+        MultiAsset multiAsset1 = MultiAsset.builder()
+                .policyId(policy1.getPolicyId())
+                .assets(List.of(
+                        Asset.builder()
+                                .name("abc")
+                                .value(BigInteger.valueOf(100))
+                                .build()
+                )).build();
+
+        MultiAsset multiAsset2 = MultiAsset.builder()
+                .policyId(policy1.getPolicyId())
+                .assets(List.of(
+                        Asset.builder()
+                                .name("xyz")
+                                .value(BigInteger.valueOf(200))
+                                .build()
+                )).build();
+
+        TxBuilderContext context = new TxBuilderContext(backendService);
+        Transaction transaction = new Transaction();
+
+        MintCreators.mintCreator(policy1, multiAsset1, true)
+                .andThen(MintCreators.mintCreator(policy1, multiAsset2, true))
+                .accept(context, transaction);
+
+        assertThat(transaction.getBody().getMint()).contains(multiAsset1, multiAsset2);
+        assertThat(transaction.getWitnessSet().getNativeScripts()).hasSize(1);
+        assertThat(transaction.getAuxiliaryData().getNativeScripts()).hasSize(1);
+        assertThat(transaction.getWitnessSet().getNativeScripts()).contains(policy1.getPolicyScript());
+        assertThat(transaction.getAuxiliaryData().getNativeScripts()).contains(policy1.getPolicyScript());
+    }
+}
+
