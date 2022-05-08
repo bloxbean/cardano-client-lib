c0df5cfc7636dad8addc1c57d5ebb56831368ea8
Dudi Edri <edridudi@gmail.com>
2022-05-08 03:01:53 +0300

    DataSignature Jackson Serialization & Deserialization

--- src/main/java/com/bloxbean/cardano/client/cip/cip30/DataSignature.java
+++ src/main/java/com/bloxbean/cardano/client/cip/cip30/DataSignature.java
@@ -4,22 +4,27 @@ import com.bloxbean.cardano.client.cip.cip8.COSEKey;
 import com.bloxbean.cardano.client.cip.cip8.COSESign1;
 import com.bloxbean.cardano.client.util.HexUtil;
 import com.bloxbean.cardano.client.util.JsonFieldWriter;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
-@Accessors(fluent = true)
 @Data
+@Accessors(fluent = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class DataSignature implements JsonFieldWriter {
+
     //cbor COSESign1
     private String signature;
     //cbor COSEKey
     private String key;
 
+    @JsonIgnore
     public COSESign1 getCOSESign1() {
         if (signature != null)
             return COSESign1.deserialize(HexUtil.decodeHexString(signature));
@@ -27,6 +32,7 @@ public class DataSignature implements JsonFieldWriter {
             return null;
     }
 
+    @JsonIgnore
     public COSEKey getCOSEKey() {
         if (key != null)
             return COSEKey.deserialize(HexUtil.decodeHexString(key));
--- src/test/java/com/bloxbean/cardano/client/cip/cip30/DataSignatureTest.java
+++ src/test/java/com/bloxbean/cardano/client/cip/cip30/DataSignatureTest.java
@@ -1,6 +1,7 @@
 package com.bloxbean.cardano.client.cip.cip30;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,5 +18,14 @@ class DataSignatureTest {
         DataSignature ds = DataSignature.from(json);
         assertThat(ds.signature()).isEqualTo("845846a2012767616464726573735839003175d03902583e82037438cc86732f6e539f803f9a8b2d4ee164b9d0c77e617030631811f60a1f8a8be26d65a57ff71825b336cc6b76361da166686173686564f44b48656c6c6f20576f726c64584036c2151e1230364b0bf9e40cb65dbdca4c5decf4187e3c5511945d410ea59a1e733b5e68178c234979053ed75b0226ba826fb951c5a79fabf10bddcabda8dc05");
         assertThat(ds.key()).isEqualTo("a4010103272006215820a5f73966e73d0bb9eadc75c5857eafd054a0202d716ac6dde00303ee9c0019e3");
+    }
+
+    @Test
+    void serializationDeserializationTest() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        DataSignature dataSignature = new DataSignature("845846a2012767616464726573735839003175d03902583e82037438cc86732f6e539f803f9a8b2d4ee164b9d0c77e617030631811f60a1f8a8be26d65a57ff71825b336cc6b76361da166686173686564f44b48656c6c6f20576f726c64584036c2151e1230364b0bf9e40cb65dbdca4c5decf4187e3c5511945d410ea59a1e733b5e68178c234979053ed75b0226ba826fb951c5a79fabf10bddcabda8dc05", "a4010103272006215820a5f73966e73d0bb9eadc75c5857eafd054a0202d716ac6dde00303ee9c0019e3");
+        String jsonString = objectMapper.writeValueAsString(dataSignature);
+        DataSignature dataSignature1 = objectMapper.readValue(jsonString, DataSignature.class);
+        assertThat(dataSignature).isEqualTo(dataSignature1);
     }
 }
