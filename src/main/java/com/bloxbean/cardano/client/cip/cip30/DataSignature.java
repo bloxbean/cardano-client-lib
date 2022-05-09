42c8540cba80ae3bd7332d96798a40fd5a6dcf15
Satya <35016438+satran004@users.noreply.github.com>
2022-05-09 18:05:23 +0800

    Helper methods in DataSignature (#116)
    * - Added constant file for CIP30 values
    
    * - Helper methods to get headers
    
    * - Added methods to get address, crv, pub key from DataSignature

--- src/main/java/com/bloxbean/cardano/client/cip/cip30/DataSignature.java
+++ src/main/java/com/bloxbean/cardano/client/cip/cip30/DataSignature.java
@@ -7,15 +7,14 @@ import com.bloxbean.cardano.client.util.JsonFieldWriter;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.Accessors;
+
+import static com.bloxbean.cardano.client.cip.cip30.CIP30Constant.*;
 
 @Data
 @Accessors(fluent = true)
 @NoArgsConstructor
-@AllArgsConstructor
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class DataSignature implements JsonFieldWriter {
 
@@ -25,17 +24,63 @@ public class DataSignature implements JsonFieldWriter {
     private String key;
 
     @JsonIgnore
-    public COSESign1 getCOSESign1() {
-        if (signature != null)
-            return COSESign1.deserialize(HexUtil.decodeHexString(signature));
+    @EqualsAndHashCode.Exclude
+    @Setter(AccessLevel.NONE)
+    private COSESign1 coseSign1;
+
+    @JsonIgnore
+    @EqualsAndHashCode.Exclude
+    @Setter(AccessLevel.NONE)
+    private COSEKey coseKey;
+
+    public DataSignature(@NonNull String signature, @NonNull String key) {
+        this.signature(signature);
+        this.key(key);
+    }
+
+    public DataSignature signature(@NonNull String signature) {
+        this.signature = signature;
+        this.coseSign1 = COSESign1.deserialize(HexUtil.decodeHexString(signature));
+
+        return this;
+    }
+
+    public DataSignature key(@NonNull String key) {
+        this.key = key;
+        this.coseKey = COSEKey.deserialize(HexUtil.decodeHexString(key));
+
+        return this;
+    }
+
+    /**
+     * Get bytes of the address set in "address" header of COSE_Sign1
+     * @return address bytes
+     */
+    public byte[] address() {
+        if (coseSign1 != null)
+            return coseSign1.headers()._protected().getAsHeaderMap().otherHeaderAsBytes(ADDRESS_KEY);
         else
             return null;
     }
 
-    @JsonIgnore
-    public COSEKey getCOSEKey() {
-        if (key != null)
-            return COSEKey.deserialize(HexUtil.decodeHexString(key));
+    /**
+     * Get crv header of COSE_Key
+     * @return crv
+     */
+    public Integer crv() {
+        if (coseKey != null)
+            return (int)coseKey.otherHeaderAsLong(CRV_KEY);
+        else
+            return null;
+    }
+
+    /**
+     * Get x header/public key bytes of COSE_Key
+     * @return public key bytes of the key used to sign the Sig_Structure
+     */
+    public byte[] x() {
+        if (coseKey != null)
+            return coseKey.otherHeaderAsBytes(X_KEY);
         else
             return null;
     }
