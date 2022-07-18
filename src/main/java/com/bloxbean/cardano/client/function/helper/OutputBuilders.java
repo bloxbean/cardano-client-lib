4f1079b523fb6b216e892f3f8f0cb03e6e9961cb
Satya <satran004@gmail.com>
2022-07-18 21:08:27 +0800

    Refactoring

--- src/main/java/com/bloxbean/cardano/client/function/helper/OutputBuilders.java
+++ src/main/java/com/bloxbean/cardano/client/function/helper/OutputBuilders.java
@@ -103,9 +103,6 @@ public class OutputBuilders {
 
             String address = txnOutput.getAddress();
             Value value = txnOutput.getValue();
-            byte[] datumHash = txnOutput.getDatumHash();
-            PlutusData inlineDatum = txnOutput.getInlineDatum();
-            byte[] scriptRef = txnOutput.getScriptRef();
 
             //If it's a mint output, add it to the context so that, it's not considered during input building
             if (isMintOutput) {
@@ -121,21 +118,12 @@ public class OutputBuilders {
                     .ifPresentOrElse(to -> {
                         Value newValue = to.getValue().plus(value);
                         to.setValue(newValue);
+                        copyDatumAndScriptRef(txnOutput, to);
 
                         checkIfMinAdaIsThere(context, to, minAdaChecker);
                     }, () -> {
                         TransactionOutput output = new TransactionOutput(address, value);
-                        if (datumHash != null && datumHash.length > 0) {
-                            output.setDatumHash(datumHash);
-                        }
-
-                        if (inlineDatum != null) {
-                            output.setInlineDatum(inlineDatum);
-                        }
-
-                        if (scriptRef != null) {
-                            output.setScriptRef(scriptRef);
-                        }
+                        copyDatumAndScriptRef(txnOutput, output);
 
                         checkIfMinAdaIsThere(context, output, minAdaChecker);
                         outputs.add(output);
@@ -158,25 +146,12 @@ public class OutputBuilders {
                 .ifPresentOrElse(to -> {
                     Value newValue = to.getValue().plus(new Value(BigInteger.ZERO, List.of(multiAsset)));
                     to.setValue(newValue);
+                    copyDatumAndScriptRef(output, to);
 
                     checkIfMinAdaIsThere(tc, to, minAdaChecker);
                 }, () -> {
                     TransactionOutput to = new TransactionOutput(output.getAddress(), new Value(BigInteger.ZERO, List.of(multiAsset)));
-                    if (output.getDatum() != null) {
-                        if (output.isInlineDatum()) {
-                            to.setInlineDatum(output.getDatum());
-                        } else {
-                            try {
-                                to.setDatumHash(output.getDatum().getDatumHashAsBytes());
-                            } catch (CborSerializationException | CborException e) {
-                                throw new TxBuildException("Unable to get dataum hash from plutus data");
-                            }
-                        }
-                    }
-
-                    if (output.getScriptRef() != null) {
-                        to.setScriptRef(output.getScriptRef());
-                    }
+                    copyDatumAndScriptRef(output, to);
 
                     checkIfMinAdaIsThere(tc, to, minAdaChecker);
                     outputs.add(to);
@@ -191,32 +166,52 @@ public class OutputBuilders {
                 .ifPresentOrElse(to -> {
                             BigInteger newCoinAmt = to.getValue().getCoin().add(output.getQty());
                             to.getValue().setCoin(newCoinAmt);
+                            copyDatumAndScriptRef(output, to);
 
                             checkIfMinAdaIsThere(tc, to, minAdaChecker);
                         },
                         () -> {
                             TransactionOutput to = new TransactionOutput(output.getAddress(), new Value(output.getQty(), new ArrayList<>()));
-                            if (output.getDatum() != null) {
-                                if (output.isInlineDatum()) {
-                                    to.setInlineDatum(output.getDatum());
-                                } else {
-                                    try {
-                                        to.setDatumHash(output.getDatum().getDatumHashAsBytes());
-                                    } catch (CborSerializationException | CborException e) {
-                                        throw new TxBuildException("Unable to get dataum hash from plutus data");
-                                    }
-                                }
-                            }
-
-                            //TODO - unit test pending
-                            if (output.getScriptRef() != null) {
-                                to.setScriptRef(output.getScriptRef());
-                            }
+                            copyDatumAndScriptRef(output, to);
 
                             checkIfMinAdaIsThere(tc, to, minAdaChecker);
                             outputs.add(to);
                         }
                 );
+    }
+
+    private static void copyDatumAndScriptRef(Output output, TransactionOutput to) {
+        if (output.getDatum() != null && to.getDatumHash() == null && to.getInlineDatum() == null) {
+            if (output.isInlineDatum()) {
+                to.setInlineDatum(output.getDatum());
+            } else {
+                try {
+                    to.setDatumHash(output.getDatum().getDatumHashAsBytes());
+                } catch (CborSerializationException | CborException e) {
+                    throw new TxBuildException("Unable to get dataum hash from plutus data");
+                }
+            }
+        }
+
+        if (output.getScriptRef() != null && to.getScriptRef() == null) {
+            to.setScriptRef(output.getScriptRef());
+        }
+    }
+
+    private static void copyDatumAndScriptRef(TransactionOutput fromOutput, TransactionOutput toOutput) {
+        if (fromOutput.getDatumHash() != null && fromOutput.getDatumHash().length > 0
+                && toOutput.getDatumHash() == null)
+        {
+            toOutput.setDatumHash(fromOutput.getDatumHash());
+        }
+
+        if (fromOutput.getInlineDatum() != null && toOutput.getInlineDatum() == null) {
+            toOutput.setInlineDatum(fromOutput.getInlineDatum());
+        }
+
+        if (fromOutput.getScriptRef() != null && toOutput.getScriptRef() == null) {
+            toOutput.setScriptRef(fromOutput.getScriptRef());
+        }
     }
 
     private static void checkIfMinAdaIsThere(TxBuilderContext tc, TransactionOutput output, MinAdaChecker minAdaChecker) {
