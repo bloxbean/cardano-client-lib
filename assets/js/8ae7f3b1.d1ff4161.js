c166964c66a46d759642675416ca8ae1c9a020c4
satran004 <satran004@users.noreply.github.com>
2022-10-18 15:08:00 +0000

    deploy: 45cf78c1d0a073af0a229f3488df8f30c0b28e7a

---
+++ assets/js/8ae7f3b1.d1ff4161.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[212],{8429:e=>{e.exports=JSON.parse('{"title":"Guides","slug":"/category/guides","permalink":"/docs/category/guides","navigation":{"previous":{"title":"Build","permalink":"/docs/gettingstarted/build"},"next":{"title":"NFT Minting","permalink":"/docs/Guides/nft-minting"}}}')}}]);
