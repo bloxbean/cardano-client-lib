dcc7a3507650fb7e7c0fd2aaea81c6b38bf30c8d
Satya <satran004@gmail.com>
2022-11-28 19:28:53 +0800

    Typo PREPOD to PREPROD

--- backend-modules/blockfrost/src/main/java/com/bloxbean/cardano/client/backend/blockfrost/common/Constants.java
+++ backend-modules/blockfrost/src/main/java/com/bloxbean/cardano/client/backend/blockfrost/common/Constants.java
@@ -3,6 +3,6 @@ package com.bloxbean.cardano.client.backend.blockfrost.common;
 public class Constants {
     public final static String BLOCKFROST_MAINNET_URL = "https://cardano-mainnet.blockfrost.io/api/v0/";
     public final static String BLOCKFROST_TESTNET_URL = "https://cardano-testnet.blockfrost.io/api/v0/";
-    public final static String BLOCKFROST_PREPOD_URL = "https://cardano-preprod.blockfrost.io/api/v0/";
+    public final static String BLOCKFROST_PREPROD_URL = "https://cardano-preprod.blockfrost.io/api/v0/";
     public final static String BLOCKFROST_PREVIEW_URL = "https://cardano-preview.blockfrost.io/api/v0/";
 }
