d09a4bc106dbdde888805dfa28afeecefe672201
Satya <satran004@gmail.com>
2022-12-24 13:54:58 +0800

    Added deserilization of script ref bytes

--- core/src/main/java/com/bloxbean/cardano/client/transaction/spec/PlutusScript.java
+++ core/src/main/java/com/bloxbean/cardano/client/transaction/spec/PlutusScript.java
@@ -2,10 +2,18 @@ package com.bloxbean.cardano.client.transaction.spec;
 
 import co.nstant.in.cbor.CborDecoder;
 import co.nstant.in.cbor.CborException;
+import co.nstant.in.cbor.model.Array;
 import co.nstant.in.cbor.model.ByteString;
 import co.nstant.in.cbor.model.DataItem;
+import co.nstant.in.cbor.model.UnsignedInteger;
+import com.bloxbean.cardano.client.common.cbor.CborSerializationUtil;
+import com.bloxbean.cardano.client.exception.CborDeserializationException;
+import com.bloxbean.cardano.client.exception.CborRuntimeException;
 import com.bloxbean.cardano.client.exception.CborSerializationException;
+import com.bloxbean.cardano.client.transaction.spec.script.NativeScript;
 import com.bloxbean.cardano.client.transaction.spec.script.Script;
+import com.bloxbean.cardano.client.transaction.spec.script.ScriptAll;
+import com.bloxbean.cardano.client.transaction.spec.script.ScriptPubkey;
 import com.bloxbean.cardano.client.util.HexUtil;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -46,4 +54,32 @@ public abstract class PlutusScript implements Script {
         return serializeAsDataItem().getBytes();
     }
 
+    //TODO -- Don't add
+    /**
+     * Deserialize to {@link PlutusV1Script} or {@link PlutusV2Script} based on the type in script reference serialized bytes.
+     * The serializedPlutusScript parameter contains both type and script body.
+     * @param serializedPlutusScript
+     * @return PlutusV1Script or PlutusV2Script
+     */
+    public static PlutusScript deserializeScriptRefBytes(byte[] serializedPlutusScript) {
+        Array plutusScriptArray = (Array) CborSerializationUtil.deserialize(serializedPlutusScript);
+        List<DataItem> dataItemList = plutusScriptArray.getDataItems();
+        if (dataItemList == null || dataItemList.size() == 0) {
+            throw new CborRuntimeException("PlutusScript deserialization failed. Invalid no of DataItem");
+        }
+
+        int type = ((UnsignedInteger) dataItemList.get(0)).getValue().intValue();
+        ByteString scriptBytes = ((ByteString) dataItemList.get(1));
+        try {
+            if (type == 1) {
+                return PlutusV1Script.deserialize(scriptBytes);
+            } else if (type == 2) {
+                return PlutusV2Script.deserialize(scriptBytes);
+            } else {
+                throw new CborRuntimeException("Invalid type : " + type);
+            }
+        } catch (Exception e) {
+            throw new CborRuntimeException("PlutusScript deserialization failed.", e);
+        }
+    }
 }
