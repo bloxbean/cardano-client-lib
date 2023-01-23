d3a9d5c00256330a3e6d44eccb7b9ab9b6099e72
Satya <satran004@gmail.com>
2023-01-23 14:06:49 +0800

    [#209] Moved Value ser/deser code from TransactionOutput to Value class

--- core/src/main/java/com/bloxbean/cardano/client/transaction/spec/Value.java
+++ core/src/main/java/com/bloxbean/cardano/client/transaction/spec/Value.java
@@ -1,11 +1,9 @@
 package com.bloxbean.cardano.client.transaction.spec;
 
-import co.nstant.in.cbor.model.Array;
-import co.nstant.in.cbor.model.DataItem;
-import co.nstant.in.cbor.model.Map;
+import co.nstant.in.cbor.model.Number;
+import co.nstant.in.cbor.model.*;
 import com.bloxbean.cardano.client.api.model.Amount;
 import com.bloxbean.cardano.client.common.cbor.custom.SortedMap;
-import com.bloxbean.cardano.client.exception.CborSerializationException;
 import com.bloxbean.cardano.client.transaction.util.CborSerializationUtil;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -32,10 +30,36 @@ public class Value {
     @Builder.Default
     private List<MultiAsset> multiAssets = new ArrayList<>();
 
-    public static Value deserialize(Array valueArray) {
+    /**
+     * Deserialize a cbor DataItem to Value
+     *
+     * @param valueItem
+     * @return {@link Value}
+     */
+    public static Value deserialize(DataItem valueItem) {
+        Value value = null;
+        if (MajorType.UNSIGNED_INTEGER.equals(valueItem.getMajorType()) || MajorType.NEGATIVE_INTEGER.equals(valueItem.getMajorType())) {
+            value = new Value();
+            value.setCoin(((Number) valueItem).getValue());
+        } else if (MajorType.BYTE_STRING.equals(valueItem.getMajorType())) { //For BigNum. >  2 pow 64 Tag 2
+            if (valueItem.getTag().getValue() == 2) {
+                value = new Value();
+                value.setCoin(new BigInteger(((ByteString) valueItem).getBytes()));
+            } else if (valueItem.getTag().getValue() == 3) {
+                value = new Value();
+                value.setCoin(new BigInteger(((ByteString) valueItem).getBytes()).multiply(BigInteger.valueOf(-1)));
+            }
+        } else if (MajorType.ARRAY.equals(valueItem.getMajorType())) {
+            Array coinAssetArray = (Array) valueItem;
+            value = Value.deserializeValueArray(coinAssetArray);
+        }
+
+        return value;
+    }
+
+    private static Value deserializeValueArray(Array valueArray) {
         //There can be two elements
         //First one coin
-
         Value value = new Value();
         List<DataItem> valueDataItems = valueArray.getDataItems();
         if (valueDataItems != null && valueDataItems.size() == 2) {
@@ -51,10 +75,51 @@ public class Value {
                 }
             }
         }
-
         return value;
     }
 
+    /**
+     * Serialize a {@link Value} object to cbor DataItem
+     *
+     * @return DataItem
+     */
+    public DataItem serialize() {
+        if (this.getMultiAssets() != null && this.getMultiAssets().size() > 0) {
+            Array coinAssetArray = new Array();
+
+            if (this.getCoin() != null) {
+                if (this.getCoin().compareTo(BigInteger.ZERO) == 0 || this.getCoin().compareTo(BigInteger.ZERO) == 1) {
+                    coinAssetArray.add(new UnsignedInteger(this.getCoin()));
+                } else {
+                    coinAssetArray.add(new NegativeInteger(this.getCoin()));
+                }
+            } else {
+                coinAssetArray.add(new UnsignedInteger(BigInteger.ZERO));
+            }
+
+            Map valueMap = this.serializeMultiAssets();
+            coinAssetArray.add(valueMap);
+            return coinAssetArray;
+        } else {
+            DataItem valueDI;
+            if (this.getCoin() != null) {
+                if (this.getCoin().compareTo(BigInteger.ZERO) == 0 || this.getCoin().compareTo(BigInteger.ZERO) == 1) {
+                    valueDI = new UnsignedInteger(this.getCoin());
+                } else {
+                    valueDI = new NegativeInteger(this.getCoin());
+                }
+            } else {
+                valueDI = new UnsignedInteger(BigInteger.ZERO);
+            }
+            return valueDI;
+        }
+    }
+
+    /**
+     * Get a {@link java.util.HashMap} of PolicyId to HashMap of assetName and {@link Asset}
+     *
+     * @return Map
+     */
     public HashMap<String, HashMap<String, BigInteger>> toMap() {
         HashMap<String, HashMap<String, BigInteger>> multiAssetsMap = new HashMap<>();
         for (MultiAsset multiAsset : multiAssets) {
@@ -67,7 +132,7 @@ public class Value {
         return multiAssetsMap;
     }
 
-    public Map serialize() throws CborSerializationException {
+    private Map serializeMultiAssets() {
         Map map = new SortedMap();
         if (multiAssets != null) {
             List<MultiAsset> cloneMultiAssets = new ArrayList<>(multiAssets);
@@ -117,7 +182,7 @@ public class Value {
         for (MultiAsset multiAsset : getMultiAssets()) {
             String policyId = multiAsset.getPolicyId();
             for (com.bloxbean.cardano.client.transaction.spec.Asset asset : multiAsset.getAssets()) {
-                amounts.add(new Amount(policyId+asset.getNameAsHex().replace("0x",""), asset.getValue()));
+                amounts.add(new Amount(policyId + asset.getNameAsHex().replace("0x", ""), asset.getValue()));
             }
         }
         return amounts;
