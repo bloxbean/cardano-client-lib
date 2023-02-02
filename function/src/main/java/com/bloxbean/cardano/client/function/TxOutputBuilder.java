24d7d41d49605ec9d81ee6188fc5dd4c88d07eec
Dudi Edri <edridudi@gmail.com>
2023-02-02 16:29:41 +0200

    Merge ChangeOutput with Transaction Outputs if it is for the same address. (saves Fee)

--- function/src/main/java/com/bloxbean/cardano/client/function/TxOutputBuilder.java
+++ function/src/main/java/com/bloxbean/cardano/client/function/TxOutputBuilder.java
@@ -23,6 +23,7 @@ public interface TxOutputBuilder {
         };
     }
 
+    @Deprecated
     default TxBuilder buildInputs(TxInputBuilder after) {
         return (context, transaction) -> {
             List<TransactionInput> inputs = new ArrayList<>();
@@ -31,18 +32,65 @@ public interface TxOutputBuilder {
             this.accept(context, outputs);
             TxInputBuilder.Result inputBuilderResult = after.apply(context, outputs);
 
-            if (inputs.size() > 0)
+            if (!inputs.isEmpty())
                 transaction.getBody().getInputs().addAll(inputs);
             transaction.getBody().getInputs().addAll(inputBuilderResult.inputs);
 
-            if (outputs.size() > 0)
+            if (!outputs.isEmpty())
                 transaction.getBody().getOutputs().addAll(outputs);
-            if (inputBuilderResult.changes != null && inputBuilderResult.changes.size() > 0)
-                transaction.getBody().getOutputs().addAll(inputBuilderResult.changes);
+            if (inputBuilderResult.changes != null && !inputBuilderResult.changes.isEmpty())
+                inputBuilderResult.changes.forEach(txOutput -> {
+                    TransactionOutput txOutputSameAddress = transaction.getBody().
+                            getOutputs()
+                            .stream()
+                            .filter(transactionOutput -> transactionOutput.getAddress().equals(txOutput.getAddress()))
+                            .findFirst().orElse(null);
+                    if (txOutputSameAddress != null) {
+                        txOutputSameAddress.setValue(txOutputSameAddress.getValue().plus(txOutput.getValue()));
+                    } else {
+                        transaction.getBody().getOutputs().add(txOutput);
+                    }
+                });
 
             //Clear multiasset in the context
             context.clearMintMultiAssets();
         };
     }
 
+    default TxBuilder buildInputs(TxInputBuilder after, boolean separateChangeOutput) {
+        return (context, transaction) -> {
+            List<TransactionInput> inputs = new ArrayList<>();
+            List<TransactionOutput> outputs = new ArrayList<>();
+
+            this.accept(context, outputs);
+            TxInputBuilder.Result inputBuilderResult = after.apply(context, outputs);
+
+            if (!inputs.isEmpty())
+                transaction.getBody().getInputs().addAll(inputs);
+            transaction.getBody().getInputs().addAll(inputBuilderResult.inputs);
+
+            if (!outputs.isEmpty())
+                transaction.getBody().getOutputs().addAll(outputs);
+            if (inputBuilderResult.changes != null && !inputBuilderResult.changes.isEmpty()) {
+                if (separateChangeOutput) {
+                    transaction.getBody().getOutputs().addAll(inputBuilderResult.changes);
+                } else {
+                    inputBuilderResult.changes.forEach(txOutput -> {
+                        TransactionOutput txOutputSameAddress = transaction.getBody().
+                                getOutputs()
+                                .stream()
+                                .filter(transactionOutput -> transactionOutput.getAddress().equals(txOutput.getAddress()))
+                                .findFirst().orElse(null);
+                        if (txOutputSameAddress != null) {
+                            txOutputSameAddress.setValue(txOutputSameAddress.getValue().plus(txOutput.getValue()));
+                        } else {
+                            transaction.getBody().getOutputs().add(txOutput);
+                        }
+                    });
+                }
+            }
+            //Clear multiasset in the context
+            context.clearMintMultiAssets();
+        };
+    }
 }
