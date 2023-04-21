757c21a4895b8178d02a5e73b00bae5cc9f2548e
Satya <satran004@gmail.com>
2023-04-21 12:47:35 +0800

    fix(api): #239 Added annotation to ignore unknown properties

--- core/src/main/java/com/bloxbean/cardano/client/api/model/Amount.java
+++ core/src/main/java/com/bloxbean/cardano/client/api/model/Amount.java
@@ -1,5 +1,8 @@
 package com.bloxbean.cardano.client.api.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
 
 import java.math.BigInteger;
@@ -9,6 +12,8 @@ import java.math.BigInteger;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class Amount {
 
     private String unit;
