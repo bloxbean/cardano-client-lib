5f5a2c584c342f4a47944cd2ab08c06e396e6055
Satya <satran004@gmail.com>
2023-08-15 21:33:31 +0800

    fix: #295 Class cast exception during evaluateTx.

--- backend-modules/ogmios/src/it/java/com/bloxbean/cardano/client/backend/ogmios/OgmiosBaseTest.java
+++ backend-modules/ogmios/src/it/java/com/bloxbean/cardano/client/backend/ogmios/OgmiosBaseTest.java
@@ -5,9 +5,12 @@ import com.bloxbean.cardano.client.backend.kupo.KupoUtxoService;
 public class OgmiosBaseTest {
     protected OgmiosBackendService ogmiosBackendService;
     protected KupoUtxoService kupoUtxoService;
+    protected KupmiosBackendService kupmiosBackendService;
 
     public OgmiosBaseTest() {
         this.ogmiosBackendService = new OgmiosBackendService("ws://ogmios-preprod:1337/");
         this.kupoUtxoService = new KupoUtxoService("http://ogmios-preprod:1442");
+
+        this.kupmiosBackendService = new KupmiosBackendService("ws://ogmios-preprod:1337/", "http://ogmios-preprod:1442");
     }
 }
