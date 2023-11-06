1ad6e87869a5f95b62c071b69901af96256e1409
Satya <satran004@gmail.com>
2023-11-06 16:22:03 +0800

    fix: #337 Added a separate method to sign transaction bytes

--- transaction-spec/src/main/java/com/bloxbean/cardano/client/transaction/TransactionSigner.java
+++ transaction-spec/src/main/java/com/bloxbean/cardano/client/transaction/TransactionSigner.java
@@ -1,6 +1,8 @@
 package com.bloxbean.cardano.client.transaction;
 
+import co.nstant.in.cbor.CborDecoder;
 import co.nstant.in.cbor.CborException;
+import co.nstant.in.cbor.model.*;
 import com.bloxbean.cardano.client.common.cbor.CborSerializationUtil;
 import com.bloxbean.cardano.client.crypto.Blake2bUtil;
 import com.bloxbean.cardano.client.crypto.KeyGenUtil;
@@ -18,7 +20,9 @@ import com.bloxbean.cardano.client.transaction.spec.TransactionBody;
 import com.bloxbean.cardano.client.transaction.spec.TransactionWitnessSet;
 import com.bloxbean.cardano.client.transaction.spec.VkeyWitness;
 
+import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
+import java.util.List;
 
 import static com.bloxbean.cardano.client.transaction.util.TransactionUtil.createCopy;
 
@@ -119,6 +123,98 @@ public enum TransactionSigner {
         cloneTxn.getWitnessSet().getVkeyWitnesses().add(vkeyWitness);
 
         return cloneTxn;
+    }
+
+    /**
+     * Sign transaction bytes with a secret key. Use this method to sign transaction bytes from another
+     * transaction builder.
+     *
+     * @param transactionBytes
+     * @param secretKey
+     * @return Signed transaction bytes
+     */
+    public byte[] sign(byte[] transactionBytes, SecretKey secretKey) {
+        byte[] txnBody = extractTransactionBody(transactionBytes);
+        byte[] txnBodyHash = Blake2bUtil.blake2bHash256(txnBody);
+
+        SigningProvider signingProvider = CryptoConfiguration.INSTANCE.getSigningProvider();
+        VerificationKey verificationKey;
+        byte[] signature;
+
+        if (secretKey.getBytes().length == 64) { //extended pvt key (most prob for regular account)
+            //check for public key
+            byte[] vBytes = HdKeyGenerator.getPublicKey(secretKey.getBytes());
+            signature = signingProvider.signExtended(txnBodyHash, secretKey.getBytes(), vBytes);
+
+            try {
+                verificationKey = VerificationKey.create(vBytes);
+            } catch (CborSerializationException e) {
+                throw new CborRuntimeException("Unable to get verification key from secret key", e);
+            }
+        } else {
+            signature = signingProvider.sign(txnBodyHash, secretKey.getBytes());
+            try {
+                verificationKey = KeyGenUtil.getPublicKeyFromPrivateKey(secretKey);
+            } catch (CborSerializationException e) {
+                throw new CborRuntimeException("Unable to get verification key from SecretKey", e);
+            }
+        }
+
+        byte[] signedTransaction = addWitnessToTransaction(transactionBytes, verificationKey.getBytes(), signature);
+        return signedTransaction;
+    }
+
+    /**
+     * Extract transaction body from transaction bytes
+     *
+     * @param txBytes
+     * @return transaction body bytes
+     */
+    private byte[] extractTransactionBody(byte[] txBytes) {
+        ByteArrayInputStream bais = new ByteArrayInputStream(txBytes);
+        CborDecoder decoder = new CborDecoder(bais);
+        try {
+            Array txArray = (Array) decoder.decodeNext();
+            DataItem txBodyDI = txArray.getDataItems().get(0);
+            return CborSerializationUtil.serialize(txBodyDI, false);
+        } catch (CborException e) {
+            throw new CborRuntimeException(e);
+        } finally {
+            try {
+                bais.close();
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    private byte[] addWitnessToTransaction(byte[] txBytes, byte[] vkey, byte[] signature) {
+        Array txDIArray = (Array) CborSerializationUtil.deserialize(txBytes);
+        List<DataItem> txDIList = txDIArray.getDataItems();
+
+        try {
+            DataItem witnessSetDI = txDIList.get(1);
+            Map witnessSetMap = (Map) witnessSetDI;
+
+            DataItem vkWitnessArrayDI = witnessSetMap.get(new UnsignedInteger(0));
+            Array vkWitnessArray;
+            if (vkWitnessArrayDI != null) {
+                vkWitnessArray = (Array) vkWitnessArrayDI;
+            } else {
+                vkWitnessArray = new Array();
+                witnessSetMap.put(new UnsignedInteger(0), vkWitnessArray);
+            }
+
+            //Add witness
+            Array vkeyWitness = new Array();
+            vkeyWitness.add(new ByteString(vkey));
+            vkeyWitness.add(new ByteString(signature));
+
+            vkWitnessArray.add(vkeyWitness);
+
+            return CborSerializationUtil.serialize(txDIArray, false);
+        } catch (CborException e) {
+            throw new CborRuntimeException(e);
+        }
     }
 
 }
