b284f128afd145c889c9e6665028b401938135b1
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2024-01-09 16:27:49 +0100

    #351 Added json utf8 converter to PlutusDataJsonConverter.java

--- plutus/src/main/java/com/bloxbean/cardano/client/plutus/spec/serializers/PlutusDataJsonConverter.java
+++ plutus/src/main/java/com/bloxbean/cardano/client/plutus/spec/serializers/PlutusDataJsonConverter.java
@@ -1,5 +1,7 @@
 package com.bloxbean.cardano.client.plutus.spec.serializers;
 
+import co.nstant.in.cbor.model.*;
+import com.bloxbean.cardano.client.exception.CborSerializationException;
 import com.bloxbean.cardano.client.plutus.spec.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -7,6 +9,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.NonNull;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import static com.bloxbean.cardano.client.plutus.spec.serializers.PlutusDataJsonKeys.*;
 
@@ -62,5 +68,56 @@ public class PlutusDataJsonConverter {
      */
     public static String toJson(PlutusData plutusData) throws JsonProcessingException {
         return mapper.writeValueAsString(plutusData);
+    }
+
+    public static String toUTF8Json(PlutusData plutusData) throws JsonProcessingException, CborSerializationException {
+        DataItem serializedPlutusData = plutusData.serialize();
+        Object o = parseItem(serializedPlutusData);
+        return mapper.writeValueAsString(o);
+    }
+
+    private static Object parseItem(DataItem item) {
+        Object value = "";
+        switch (item.getMajorType()) {
+            case BYTE_STRING:
+                value = bytestringItemToString(item);
+                break;
+            case ARRAY:
+                value = dataItemArrayToString((Array) item);
+                break;
+            case MAP:
+                value = parseDataItemMap((Map) item);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unkown type. Not implemented"); // TODO need to implement the other types
+        }
+        return value;
+    }
+
+    private static String bytestringItemToString(DataItem item) {
+        String value = "";
+        byte[] bytes = ((ByteString) item).getBytes();
+        value = new String(bytes);
+        return value;
+    }
+
+    private static Object dataItemArrayToString(Array array) {
+        List<Object> decodedItemList = new ArrayList<>();
+        for (DataItem dataItem : array.getDataItems()) {
+            if(!dataItem.getMajorType().equals(MajorType.SPECIAL)){
+                decodedItemList.add(parseItem(dataItem));
+            }
+        }
+        return decodedItemList.stream().toArray(Object[]::new);
+    }
+
+    private static HashMap<String, Object> parseDataItemMap(Map m) {
+        HashMap<String, Object> decodedMap = new HashMap<>();
+        for (DataItem keyItem : m.getKeys()) {
+            String keyString = (String) parseItem(keyItem);
+            Object valueString = parseItem(m.get(keyItem));
+            decodedMap.put(keyString, valueString);
+        }
+        return decodedMap;
     }
 }
