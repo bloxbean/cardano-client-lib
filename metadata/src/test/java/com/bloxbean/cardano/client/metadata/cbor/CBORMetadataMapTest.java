4259fa9e9fdebd61020ba66da84f5b0d10e81431
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2024-01-19 13:18:49 +0100

    #341 added automatic string split to max 64 bytes and added test for string splitting.

--- metadata/src/test/java/com/bloxbean/cardano/client/metadata/cbor/CBORMetadataMapTest.java
+++ metadata/src/test/java/com/bloxbean/cardano/client/metadata/cbor/CBORMetadataMapTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.math.BigInteger;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CBORMetadataMapTest {
 
@@ -17,13 +18,16 @@ class CBORMetadataMapTest {
         metadataMap.put("key1", "value1");
         metadataMap.put("key2", BigInteger.valueOf(123));
         metadataMap.put("key3", "value3");
+        metadataMap.put("key4", "Extra Long string of more than 64 Bytes................................");
         metadataMap.put(new BigInteger("123456"), "value4");
 
-        assertThat(metadataMap.keys()).hasSize(4);
+        assertTrue(metadataMap.get("key4") instanceof CBORMetadataList);
+
+        assertThat(metadataMap.keys()).hasSize(5);
         metadataMap.remove("key1");
         metadataMap.remove(new BigInteger("123456"));
 
-        assertThat(metadataMap.keys()).hasSize(2);
+        assertThat(metadataMap.keys()).hasSize(3);
         assertThat(metadataMap.get("key2")).isEqualTo(BigInteger.valueOf(123));
         assertThat(metadataMap.get("key3")).isEqualTo("value3");
 
