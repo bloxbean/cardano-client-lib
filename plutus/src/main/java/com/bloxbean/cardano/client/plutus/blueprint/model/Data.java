4928a64a906812a406ce11a0f47f092a0534c624
Satya <satran004@gmail.com>
2024-08-05 11:00:43 +0800

    chore: Added javadoc

--- plutus/src/main/java/com/bloxbean/cardano/client/plutus/blueprint/model/Data.java
+++ plutus/src/main/java/com/bloxbean/cardano/client/plutus/blueprint/model/Data.java
@@ -2,6 +2,11 @@ package com.bloxbean.cardano.client.plutus.blueprint.model;
 
 import com.bloxbean.cardano.client.plutus.spec.ConstrPlutusData;
 
+/**
+ * Implement this interface to convert an object to PlutusData and vice versa.
+ *
+ * @param <T>
+ */
 public interface Data<T> {
 
     ConstrPlutusData toPlutusData();
