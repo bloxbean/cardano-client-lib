0e7a37516bea181b06a96e89189b503801188f14
Satya <satran004@gmail.com>
2026-03-01 19:26:04 +0800

    Fix and cleanup 0.8.0 preview1 docs

--- docs/content/preview/mpftrie/_meta.js
+++ docs/content/preview/mpftrie/_meta.js
@@ -1,5 +1,8 @@
 export default {
   overview: 'Overview',
   'multi-version-gc': 'Multi-Version & Garbage Collection',
-  'production-guide': 'Production Guide'
+  'production-guide': {
+    title: 'Production Guide',
+    display: 'hidden'
+  }
 }
