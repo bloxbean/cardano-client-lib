e9f605a34a721c19096a96bc9681a80bcb4b918a
Satya <35016438+satran004@users.noreply.github.com>
2026-03-01 19:50:45 +0800

    Doc change for 0.8.0-preview features (#598)
    * Added 0.8.0 preview docs
    
    * Fix and cleanup 0.8.0 preview1 docs

---
+++ docs/content/preview/_meta.js
@@ -0,0 +1,7 @@
+export default {
+  index: "What's New in 0.8.0",
+  'unified-tx': 'Unified Tx API',
+  txflow: { title: 'TxFlow' },
+  txplan: { title: 'TxPlan (YAML Transactions)' },
+  mpftrie: { title: 'MpfTrie (Verified Data Structures)' }
+}
---
+++ docs/content/preview/mpftrie/_meta.js
@@ -0,0 +1,8 @@
+export default {
+  overview: 'Overview',
+  'multi-version-gc': 'Multi-Version & Garbage Collection',
+  'production-guide': {
+    title: 'Production Guide',
+    display: 'hidden'
+  }
+}
